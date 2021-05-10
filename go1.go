0cca99ca1378e9e32463265fead2240d4712e3de
Don Guiruela <guirueladon@gmail.com>
2021-05-10 23:52:07 +0800

    .

--- go1.go
+++ go1.go
@@ -9,7 +9,7 @@ func main() {
 	var option, search string
 	var b, n int
 
-	fmt.Println("[B]inary Search")
+	fmt.Println("[B]inary Search\n[L]inear Search")
 	fmt.Printf("Enter your choice of search: ")
 	fmt.Scan(&option)
 
@@ -17,36 +17,38 @@ func main() {
 	case "B":
 		b = 2
 		search = "Binary"
+	default:
+		b = -1
+		search = "Invalid"
 	}
 
 	fmt.Printf("Enter number of elements: ")
 	fmt.Scan(&n)
 
-	/*Binary Search
-
-	Worst-case performance	O(log n)
-	Best-case performance	O(1)
-	Average performance	O(log n)
-	Worst-case space complexity	O(1)
-	*/
-
-	x := GetLog(b, n)
+	x := GetLogBS(&b, &n)
 
 	fmt.Println(search, "Search")
 	fmt.Println("Maximum Worst Comparison: ", x)
 }
 
-func GetLog(b int, n int) int {
+/*Binary Search
+
+Worst-case performance	O(log n)
+Best-case performance	O(1)
+Average performance	O(log n)
+Worst-case space complexity	O(1)
+*/
+func GetLogBS(b *int, n *int) int {
 	var l int = 0
 
-	for n > 1 {
-		n /= b
+	for *n > 1 {
+		*n /= *b
 
-		if n > 0 {
+		if *n > 0 {
 			l += 1
 		}
 
-		if n == 1 {
+		if *n == 1 {
 			l = (l * 2) + 1
 		}
 	}
